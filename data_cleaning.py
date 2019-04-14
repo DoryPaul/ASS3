65748fccb339ef002683cfc52f47fbcb8d139f23
Huifeng Ruan <36803734+rhf0410@users.noreply.github.com>
2019-04-14 13:51:18 +1000

    Add files via upload

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import pylab as pl
 def load_file(data_path):
     df = pd.read_csv(data_path,names=['age','sex','chest pain type','resting blood pressure','serum cholestoral','fasting blood sugar',
                                       'resting electrocardiographic results','maximum heart rate','exercise induced angina',
@@ -21,4 +21,18 @@ def data_uniform(data_path):
         else:
             temp[df1.keys()[i]] = df1[df1.keys()[i]]
     return temp
+
+def input_data_uniform(data_path, data):
+    df1 = pd.read_csv(data_path,index_col=0,names=['age','sex','chest pain type','resting blood pressure','serum cholestoral','fasting blood sugar',
+                                      'resting electrocardiographic results','maximum heart rate','exercise induced angina',
+                                      'oldpeak','the slope of ST','number of major vessels','thal','target'])
+    keys = df1.keys()
+    temp = pd.DataFrame()
+    for i in range(len(keys)):
+        if df1.keys()[i] != 'target':
+            d = pd.Series(data[i])
+            temp[df1.keys()[i]] = ((d-min(df1[df1.keys()[i]]))/(max(df1[df1.keys()[i]])-min(df1[df1.keys()[i]])))
+
+    return temp
+
 load_file('processed.csv')
